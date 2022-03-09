a037bb6d48e5abb54c1c88f5b387c0127f3eb537
Dorian <dorianc.vieira@gmail.com>
2022-03-09 00:43:44 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'artigo.php';
+$artigo = new Artigo();
+$artigos = $artigo->exibirTodos();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Meu Blog</h1>
+        <?php foreach ($artigos as $artigo) : ?>
+            <h2>
+                <a href="<?php echo $artigo['link']; ?>">
+                    <?php echo $artigo['titulo']; ?>
+                </a>
+            </h2>
+            <p>
+                <?php echo $artigo['conteudo']; ?>
+            </p>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
